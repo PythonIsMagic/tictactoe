357c0392381ea565914674b1f64cad2eaaad3016
Erik Lunna <eslunna@gmail.com>
2016-04-04 20:43:48 -0500

    Added ttt.py with general pseudocode for the gameloop.

---
+++ ttt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+# Author: lunatunez
+# Date Created: Mon Apr  4 20:28:29 2016
+# Purpose: Tic Tac Toe Game
+"""
+
+
+def main():
+    pass
+
+    # Initialize a new tictactoe board
+    # Determine which player goes first
+
+    # Set gamestate to PLAYING
+
+    # Game loop
+        # Display the board
+        # If it is the computer's turn, let them compute the best move
+        # If it is the human's turn, let them choose a move.
+
+        # Execute the chosen move
+
+        # Check the state of board
+            # CPU wins, Human wins, Tie, or in progress.
+        # If the game is still in progress:
+            # Change the active player
+        #
+
+if __name__ == "__main__":
+	main()
