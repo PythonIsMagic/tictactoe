3972d3519884a275ee1b3e76f4e15b9bd049a89f
Erik Lunna <eslunna@gmail.com>
2016-08-21 23:01:23 -0400

    Added a board constructor and a __str__ method.

--- board.py
+++ board.py
@@ -1,3 +1,14 @@
+from __future__ import print_function
+
+
 class Board():
-    def __init__(self):
-        pass
+    def __init__(self, size=3):
+        self.board = [['.' for x in range(size)] for y in range(size)]
+
+    def __str__(self):
+        _str = ''
+        for row in self.board:
+            for col in row:
+                _str += str(col)
+            _str += "\n"
+        return _str
