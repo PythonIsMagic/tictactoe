b7357720d11ee3baa3abd566ec4489c98e351be2
Erik Lunna <eslunna@gmail.com>
2016-08-21 23:47:07 -0400

    Changed the board core to use a plain list instead of a double list. Simpler. Displays with room for 2 digit numbers.

--- board.py
+++ board.py
@@ -1,27 +1,30 @@
 from __future__ import print_function
+import math
 
 PLACEHOLDER = '.'
 
 
 class Board():
     def __init__(self, size=3):
-        self.board = [[PLACEHOLDER for x in range(size)] for y in range(size)]
+        #  self.board = [[PLACEHOLDER for x in range(size)] for y in range(size)]
+        self.board = [x for x in range(size ** 2)]
 
     def __str__(self):
         _str = ''
-        for row in self.board:
-            for col in row:
-                _str += str(col)
-            _str += "\n"
+
+        for i in self.board:
+            if i % math.sqrt(len(self.board)) == 0:
+                _str += '\n'
+            _str += '{:3}'.format(i)
         return _str
 
-    def play(self, x, y, piece):
-        if x < 0 or y < 0 or x >= len(self.board) or y >= len(self.board):
-            print('x or y is out of bounds!')
+    def play(self, i, piece):
+        if i < 0 or i >= len(self.board):
+            print('The index  is out of bounds!')
             return -1
 
-        if self.board[x][y] != PLACEHOLDER:
-            print('({}, {}) is already played on!'.format(x, y))
+        if self.board[i] != PLACEHOLDER:
+            print('(Spot #{} is already played on!'.format(i))
             return -1
 
-        self.board[x][y] = piece
+        self.board[i] = piece
