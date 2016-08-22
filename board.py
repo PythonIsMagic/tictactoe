751935ec325887cf604b178756de7fdde3e47769
Erik Lunna <eslunna@gmail.com>
2016-08-21 23:15:40 -0400

    Added a play function to board.py. Has error checking

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
+PLACEHOLDER = '.'
+
 
 class Board():
     def __init__(self, size=3):
-        self.board = [['.' for x in range(size)] for y in range(size)]
+        self.board = [[PLACEHOLDER for x in range(size)] for y in range(size)]
 
     def __str__(self):
         _str = ''
@@ -12,3 +14,14 @@ class Board():
                 _str += str(col)
             _str += "\n"
         return _str
+
+    def play(self, x, y, piece):
+        if x < 0 or y < 0 or x >= len(self.board) or y >= len(self.board):
+            print('x or y is out of bounds!')
+            return -1
+
+        if self.board[x][y] != PLACEHOLDER:
+            print('({}, {}) is already played on!'.format(x, y))
+            return -1
+
+        self.board[x][y] = piece
