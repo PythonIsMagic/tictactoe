041fcca72d45e163a46c70117a65647a0c6e3b86
Erik Lunna <eslunna@gmail.com>
2016-08-23 13:57:00 -0400

    Finished the working version. Board checks for win correctly. Game starts and finishes fine.

--- board.py
+++ board.py
@@ -2,29 +2,78 @@ from __future__ import print_function
 import math
 
 PLACEHOLDER = '.'
+SYMBOLS = {'o': 1, 'x': -1}
 
 
 class Board():
     def __init__(self, size=3):
-        #  self.board = [[PLACEHOLDER for x in range(size)] for y in range(size)]
         self.board = [x for x in range(size ** 2)]
+        self.length = int(math.sqrt(len(self.board)))
 
     def __str__(self):
         _str = ''
-
-        for i in self.board:
-            if i % math.sqrt(len(self.board)) == 0:
+        for i in range(len(self)):
+            if i % self.length == 0:
                 _str += '\n'
-            _str += '{:3}'.format(i)
+            _str += format(self.board[i], '^3')
+        _str += '\n'
         return _str
+
+    def __len__(self):
+        return len(self.board)
 
     def play(self, i, piece):
         if i < 0 or i >= len(self.board):
             print('The index  is out of bounds!')
-            return -1
+            return False
 
-        if self.board[i] != PLACEHOLDER:
-            print('(Spot #{} is already played on!'.format(i))
-            return -1
+        if self.board[i] in ['x', 'o']:
+            print('Spot #{} is already played on!'.format(i))
+            return False
 
         self.board[i] = piece
+        return True
+
+    def find_win(self):
+        # Searches for a complete winning row, column, or diagonal.
+        # If one is found, it returns the symbol with the win.
+        # using a counting method: 'o' = +1, 'x' = -1
+        length = int(math.sqrt(len(self.board)))
+        dndg_ct, updg_ct = 0, 0
+
+        # Columns and rows
+        for x in range(length):
+            col_ct, row_ct = 0, 0
+
+            for y in range(length):
+                # Check column
+                col = x + length * y
+                col_ct += SYMBOLS.get(self.board[col], 0)
+
+                # Check row
+                row = length * x + y
+                row_ct += SYMBOLS.get(self.board[row], 0)
+
+            # Search diagonals
+            # Check downward diagnonal
+            dndg = x * (length + 1)
+            dndg_ct += SYMBOLS.get(self.board[dndg], 0)
+
+            # Check upward diagnonal
+            start = length * (length - 1)
+            updg = start - x * (length - 1)
+            updg_ct += SYMBOLS.get(self.board[updg], 0)
+
+            # Check row/col counts
+            if row_ct == length or col_ct == length:
+                return 'o'
+            elif row_ct == -length or col_ct == -length:
+                return 'x'
+
+        if dndg_ct == length or updg_ct == length:
+            return 'o'
+        elif dndg_ct == -length or updg_ct == -length:
+            return 'x'
+
+        # If none are found, it returns None.
+        return None
--- ttt.py
+++ ttt.py
@@ -11,35 +11,63 @@ import random
 
 
 def main():
-    pass
+    print('Python Tic Tac Toe!')
+    print('~'*40)
+    print('CPU = \'o\' and human =\'x\'')
 
     # Initialize a new tictactoe board
     b = board.Board()
-    print(b)
 
     # Determine which player goes first
-    coin = random.randint(0, 1)
-    print(coin)
+    # HUMAN = 0, CPU = 1
+    player = random.randint(0, 1)
 
-    # Set gamestate to PLAYING
+    print('Flipping the coin for first go....')
+    if player == 0:
+        print('The HUMAN goes first!')
+    else:
+        print('The CPU goes first!')
+    print('~'*40)
+
     playing = True
 
-    exit()
     # Game loop
     while playing:
+        if player == 0:
+            print('Go human...')
+            print(b)
 
-        # Display the board
-        print(b)
+            while True:
+                move = input('>')
+                if b.play(int(move), 'x'):
+                    break
+                else:
+                    print('invalid move, try again')
+
         # If it is the computer's turn, let them compute the best move
-        # If it is the human's turn, let them choose a move.
+        elif player == 1:
+            print('CPU is thinking...')
 
-        # Execute the chosen move
+            while True:
+                move = random.randint(0, len(b) - 1)
+                if b.play(move, 'o'):
+                    print('The CPU played on spot {}'.format(move))
+                    break
+        else:
+            print('Player selection error! Aborting!!!!!')
+            exit()
 
         # Check the state of board
-            # CPU wins, Human wins, Tie, or in progress.
-        # If the game is still in progress:
-            # Change the active player
-        #
+        state = b.find_win()
+
+        # CPU wins, Human wins, Tie, or in progress.
+        if state == 'o' or state == 'x':
+            print('{} wins!'.format(state))
+            exit()
+
+        # Change players
+        player = (player + 1) % 2
+
 
 if __name__ == "__main__":
-	main()
+    main()
