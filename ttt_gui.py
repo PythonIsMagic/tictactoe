ce4dc332a3fe1c05479654b0e2c8114ce2752073
Erik Lunna <eslunna@gmail.com>
2016-09-21 19:52:36 -0400

    Fixed the button list in the gui, and got a working draft working that switches between human and cpu.

--- ttt_gui.py
+++ ttt_gui.py
@@ -1,32 +1,62 @@
 # coding: utf-8
 from tkinter import *
 from tkinter import ttk
+import ttt
 
 
 class TicTacToeBoard(Frame):
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
+        self.game = ttt.TicTacToe(size=3)
+
         mainframe = ttk.Frame(root, padding=(12, 12, 12, 12))
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        buttons = [Button(mainframe, text='{}'.format(x)) for x in range(9)]
 
-        for b in buttons:
-            # Worked for text labels
-            b.config(height=5, width=10)
+        self.symbol = 'X'
+        self.strVars = [StringVar() for x in range(9)]
+
+        buttons = []
+        for i, s in enumerate(self.strVars):
+            func = (lambda i=i: self.btn_press(i))
+
+            b = Button(mainframe, textvariable=s, command=func)
+            #  b.config(height=5, width=10, )
+            buttons.append(b)
 
         for x in range(3):
             for y in range(3):
-                buttons.pop(0).grid(column=y, row=x)
+                buttons.pop(0).grid(row=x, column=y)
 
         # Config padding
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5, sticky='EWNS')
+            child.config(font='size, 20')
 
-root = Tk()
-root.title("TicTacToe")
-root.config(bg='black')
-root.resizable(width=False, height=False)
+        self.check_cpu()
 
-TicTacToeBoard(root)
+    def check_cpu(self):
+        # Check if it is the CPU's turn and run their turn.
+        if self.game.player == 1:
+            cpu_move = self.game.cpu_turn()
+            #  self.btn_press(cpu_move)
+            self.strVars[cpu_move].set(self.game.sym())
+            self.game.next_player()
 
-root.mainloop()
+    def btn_press(self, i):
+        # Human turn
+        result = self.game.b.play(i, self.game.sym())
+        if result:
+            self.strVars[i].set(self.game.sym())
+            self.game.next_player()
+            self.check_cpu()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("TicTacToe")
+    root.config(bg='black')
+    root.resizable(width=False, height=False)
+
+    TicTacToeBoard(root)
+
+    root.mainloop()
