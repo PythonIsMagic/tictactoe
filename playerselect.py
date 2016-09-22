c52c20bec9380360732c5c2534cddb07d07c9585
Erik Lunna <eslunna@gmail.com>
2016-09-21 21:21:34 -0400

    Added a button to the player selection frame and a first draft send method.

--- playerselect.py
+++ playerselect.py
@@ -5,17 +5,21 @@ from tkinter import *
 class PlayerSelect(Frame):
     def __init__(self, parent=None, **options):
         Frame.__init__(self, parent, **options)
+
         self.pack()
         Label(self, text="Select your symbol")
 
         self.var = StringVar()
 
-        Radiobutton(self, text="O", command=self.onPress, variable=self.var, value='O').pack(anchor=W)
-        Radiobutton(self, text="X", command=self.onPress, variable=self.var, value='X').pack(anchor=W)
+        Radiobutton(self, text="O", font='size, 15', variable=self.var, value='O').pack(anchor=W)
+        Radiobutton(self, text="X", font='size, 15', variable=self.var, value='X').pack(anchor=W)
         self.var.set('O')
 
-    def onPress(self):
-        pass
+        Button(self, text="Play", font='size, 15', command=self.send).pack(anchor=S)
+
+    def send(self):
+        self.selection = self.var.get()
+        self.destroy()
 
 if __name__ == "__main__":
     PlayerSelect().mainloop()
