598a6899bcc26b0072f4835354548946f3762ce5
Erik Lunna <eslunna@gmail.com>
2016-09-21 21:03:12 -0400

    Added a symbol select combobox - but both are selected right now.

---
+++ playerselect.py
@@ -0,0 +1,21 @@
+from tkinter import *
+#  from tkinter import ttk
+
+
+class PlayerSelect(Frame):
+    def __init__(self, parent=None, **options):
+        Frame.__init__(self, parent, **options)
+        self.pack()
+        Label(self, text="Select your symbol")
+
+        self.var = StringVar()
+
+        Radiobutton(self, text="O", command=self.onPress, variable=self.var, value='O').pack(anchor=W)
+        Radiobutton(self, text="X", command=self.onPress, variable=self.var, value='O').pack(anchor=W)
+        self.var.set('O')
+
+    def onPress(self):
+        pass
+
+if __name__ == "__main__":
+    PlayerSelect().mainloop()
